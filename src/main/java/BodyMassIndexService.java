18e2974160044f3b704312c54b779968cd635965
Hallodri <bhallodri@gmail.com>
2021-02-25 18:23:58 +0300

    fixed #2

--- src/main/java/BodyMassIndexService.java
+++ src/main/java/BodyMassIndexService.java
@@ -1,6 +1,6 @@
 public class BodyMassIndexService {
     double calculate(int weight,double growth){
-        double bodyMassIndex =  (weight / growth);
+        double bodyMassIndex =  weight / (growth * growth) * 10_000;
         return bodyMassIndex;
     }
 }
