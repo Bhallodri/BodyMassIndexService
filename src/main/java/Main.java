0838b4587fe26c20053661e6ec5aa9e851214e74
Hallodri <bhallodri@gmail.com>
2021-02-25 18:27:38 +0300

    fixed #3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BodyMassIndexService service = new BodyMassIndexService();
-        double bodyMassIndex = service.calculate(77,172);
+        double bodyMassIndex = service.calculate(77,173);
         System.out.println(bodyMassIndex) ;
     }
 }
